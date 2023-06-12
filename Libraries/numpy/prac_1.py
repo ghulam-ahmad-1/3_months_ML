75c9a0d46089078f02264a0ea68dfde3928bb740
ghulamahma <2021ee305@student.uet.edu.pk>
2023-06-13 00:09:46 +0500

    Day 12 ends Starting Numpy

--- Libraries/numpy/prac_1.py
+++ Libraries/numpy/prac_1.py
@@ -1,6 +1,13 @@
 print("Starting Numpy")
-import numpy as nu 
-arr =nu.array([1,2,3,4,5,6,7,8,9])
-print(arr)
-a=[1,2,4,5,6,7]
-print(a)
+import numpy as np 
+print("Numpy Version : ")
+print(np.__version__)
+arr =np.array([1,2,3,4,5,6,7,8,9])
+print('Array in Numpy : ',arr)
+
+# Numpy Creating Arrays : 
+# Its a Numpy ndarray ; 
+print(type(arr))
+
+arr1=np.array((1,2,3,4,5))
+print(type(arr1), 'We Converted the Tuple into the  ndarray !')
